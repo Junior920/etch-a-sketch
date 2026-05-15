7c59d6fc960ee2aded7270e862c4886c53474b2e
Stanley <stanley44u@gmail.com>
2026-05-15 19:38:42 +0000

    finished project, added the mouseover effect and some reset button.

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const container = document.querySelector('#container');
 const btn = document.querySelector('#change-size')
-
+const resetBtn = document.querySelector('#reset')
 
 function createGrid(size) {
   container.innerHTML = "";
   const squareSize = 500 / size;
 
 
-  for (let i =0; i < size * size; i++) {
+  for (let i = 0; i < size * size; i++) {
   const square = document.createElement("div");
 
   square.classList.toggle("square");
 
   square.style.width = `${squareSize}px`;
   square.style.height = `${squareSize}px`;
+
+  square.addEventListener("mouseover", () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    
+    square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  });
 
   container.appendChild(square)
   }
@@ -27,11 +35,19 @@ function createGrid(size) {
 
     size = Number(size)
 
-    if (size > 0 && size <= 100) {
+    if (size > 15 && size <= 100) {
       createGrid(size);
     } else {
-      alert("Please enter a number between 1 and 100!");
+      alert("Please enter a number between 16 and 100!");
     }
+  });
+
+  resetBtn.addEventListener("click", () => {
+    const squares = document.querySelectorAll(".square");
+
+    squares.forEach(square => {
+      square.style.backgroundColor = "white";
+    });
   });
 
   createGrid(16);
